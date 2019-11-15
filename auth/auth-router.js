fe3a47b47ecf89e33210a2a70d8f33886bab7697
Joshua Holtsclaw <Jildsmiles@yahoo.com>
2019-11-15 11:39:41 -0500

    end points functional

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,53 @@
 const router = require('express').Router();
+const db = require('../database/dbConfig');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password,10);
+  user.password = hash;
+
+  db('users').insert(user)
+  .then(newUser => {
+    res.status(200).json(newUser) 
+  })
+  .catch(err => {
+    res.status(500).json({message:"Couldnt register user"})
+  })
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  const {username, password} = req.body;
+
+  db('users').where({username})
+  .first()
+  .then(user => {
+    if(user && bcrypt.compareSync(password, user.password)){
+      const token = getJwtToken(user);
+      res.status(200).json(token);
+    }else{
+      res.status(401).json({message:'invalid creds'})
+    }
+    
+  })
+  .catch(err => {
+    res.status(500).json({message:'login error'})
+  })
 });
+
+function getJwtToken(user){
+  const payload = {
+    id: user.id,
+    name: user.username,
+};
+const secret = process.env.JWT_SECRET || 'hehe cant see me'
+const options = {
+    expiresIn:'1h'
+};
+
+return jwt.sign(payload,secret,options) 
+}
 
 module.exports = router;
