4fb4232642a1cf8efa86844c63efeb5513a77a11
Joshua Holtsclaw <Jildsmiles@yahoo.com>
2019-11-15 12:16:54 -0500

    testing complete

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -26,7 +26,7 @@ router.post('/login', (req, res) => {
   .then(user => {
     if(user && bcrypt.compareSync(password, user.password)){
       const token = getJwtToken(user);
-      res.status(200).json(token);
+      res.status(200).json({message:`Welcome ${user.username} `,token});
     }else{
       res.status(401).json({message:'invalid creds'})
     }
